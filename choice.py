59f5d4ac24cc2986428dfbf92ef3c121e925797e
laureno12 <laurenosays@gmail.com>
2023-01-10 18:19:51 -0500

    Updated all classes with init methods

---
+++ choice.py
@@ -0,0 +1,17 @@
+class Choice:
+
+    gesture = ["rock", "paper", "scissors", "lizard", "spock"]
+    def __init__(self, gesture):
+        self.gesture = []
+
+
+# Rock crushes Scissors
+# Scissors cuts Paper 
+# Paper covers Rock
+# Rock crushes Lizard
+# Lizard poisons Spock
+# Spock smashes Scissors
+# Scissors decapitates Lizard
+# Lizard eats Paper
+# Paper disproves Spock
+# Spock vaporizes Rock
---
+++ player.py
@@ -0,0 +1,7 @@
+from choice import Choice
+
+class Player:
+
+    def __init__(self, name, number_players):
+        self.name = " "
+        self.number_players = input("How many human players? 1 or 2 ")
