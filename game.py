5e4bbc789b55a03b95e70137d1947a1e343c0039
laureno12 <laurenosays@gmail.com>
2023-01-16 10:01:33 -0500

    Created methods in Game

---
+++ game.py
@@ -0,0 +1,12 @@
+from player import Player
+from human import Human
+from ai import AI
+
+class Game
+
+def display_welcome(self):
+    self.display_welcome = print("Welcome to Rock, Paper, Scissors, Lizard, Spock!")
+def display_rules(self):
+    self.display_rules = print("Rules: Each player picks a gesture from the list to try to win the round./n Rock crushes Scissors. /n Scissors cuts Paper /n Paper covers Rock /n Rock crushes Lizard /n Lizard poisons Spock /n Spock smashes Scissors /n Scissors decapitates Lizard /n Lizard eats Paper /n Paper disproves Spock /n Spock vaporizes Rock. /n The winner of the round wins a point. Game will continue until best of 3 rounds")
+def choose_game(self): 
+def play_game(self):  
--- player.py
+++ player.py
@@ -4,7 +4,7 @@ class Player:
     def __init__(self, name):
         self.name = " "
         self.list_gestures = ["rock", "paper", "scissors", "lizard", "spock"]
-        self.seleced_gesture = ''
+        self.selected_gesture = ''
         self.wins = 0
 
     
