7a7a957d7f30cea86049dfb0f2bfe9f4ecc1487f
laureno12 <laurenosays@gmail.com>
2023-01-16 10:09:25 -0500

    created methods for AI

--- ai.py
+++ ai.py
@@ -1,3 +1,10 @@
 import random
+from player import Player
 
-class AI(player):
+class AI(Player):
+    def __init__(self) -> None:
+        super().__init__()
+    def choose_gesture(self):
+        random.choice(self.list_gestures)
+        print(f"The computer has chosen {random.choice}")
+
